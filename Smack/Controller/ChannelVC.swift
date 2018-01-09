f17452de1f914dda3d5271900437408ca7315662
LinuxPlus <linuxplus@linuxpluss-mac.local>
2018-01-08 17:49:56 -0800

    ChatVC menuBtn functionality; ChatVC positioning in ChannelVC

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,35 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by LinuxPlus on 1/8/18.
+//  Copyright © 2018 ARC. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            //size of rear view
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,45 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by LinuxPlus on 1/8/18.
+//  Copyright © 2018 ARC. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            //buttonAction
+        menuBtn.addTarget(self.revealViewControler(), action: #selector(SWRevealViewControler.revealToggle(_:)), for: .touchUpInside)
+        
+            //reveal rear view upon pan or tap
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+
+        
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
