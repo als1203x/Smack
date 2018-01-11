e32a94c009d86d79c119695b2cad4fafca6e5dcb
LinuxPlus <linuxplus@linuxpluss-mac.local>
2018-01-10 16:20:01 -0800

    Created notification Implemented random generated background color create circle button Constants update

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -15,14 +15,17 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     
     //Variables
     var avatarName = "profileDefault"
     var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    var bgColor : UIColor?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -30,21 +33,53 @@ class CreateAccountVC: UIViewController {
             userImg.image = UIImage(named: UserDataService.instance.avatarName)
             avatarName = UserDataService.instance.avatarName
         }
+        
+        if avatarName.contains("light") && bgColor == nil   {
+            userImg.backgroundColor = UIColor.lightGray
+        }
+    }
+    
+    
+    func setUpView()    {
+            //placeholder text color
+        usernameTxt.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedStringKey.foregroundColor: smackPurple])
+        passwordTxt.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor: smackPurple])
+        emailTxt.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSAttributedStringKey.foregroundColor: smackPurple])
+        spinner.isHidden = true
+        
+        //tap Gesture Recognizer
+        let tap = UITapGestureRecognizer(target: self, action: #selector(CreateAccountVC.handleTap))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func  handleTap() {
+        view.endEditing(true)
     }
 
+    //@IBActions
+    
     @IBAction func closePressed(_ sender: UIButton) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
 
     @IBAction func pickBGColorPressed(_ sender: Any) {
-        
+        let r = CGFloat( arc4random_uniform(255) / 255 )
+        let g = CGFloat( arc4random_uniform(255) / 255 )
+        let b = CGFloat( arc4random_uniform(255) / 255 )
+        bgColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        UIView.animate(withDuration: 0.2)   {
+            self.userImg.backgroundColor = self.bgColor
+        }
     }
+
     @IBAction func pickAvatarPressed(_ sender: Any) {
         
         performSegue(withIdentifier: TO_AVATAR_PICKER, sender: nil)
         
     }
     @IBAction func createAccountPressed(_ sender: UIButton) {
+        spinner.isHidden = false
+        spinner.startAnimating()
             //guard with , is a where statement
         guard let name = usernameTxt.text, usernameTxt.text != "" else { return }
         guard let email = emailTxt.text , emailTxt.text != "" else { return }
@@ -57,8 +92,10 @@ class CreateAccountVC: UIViewController {
                         if success  {
                             AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
                                 if success  {
+                                    self.spinner.isHidden = true
+                                    self.spinner.stopAnimating()
                                     self.performSegue(withIdentifier: UNWIND, sender: nil)
-                                    
+                                    NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
                                 }
                             })
                         }
---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by LinuxPlus on 1/10/18.
+//  Copyright © 2018 ARC. All rights reserved.
+//
+
+import UIKit
+
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        setUpView()
+    }
+    
+    func setUpView()    {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
+    }
+}
